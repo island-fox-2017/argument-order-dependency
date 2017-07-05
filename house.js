5897fff9273973a88bac40364ad14319dd9986f1
pdvega <pdvegaa@gmail.com>
2017-07-05 11:45:53 +0700

    named dependency update

--- house.js
+++ house.js
@@ -5,17 +5,17 @@ class House {
   constructor(param) { //dependency
     this.address = param['address']
     this.square_feet = param['square_feet']
-    this.num_bedrooms = param['num_bedrooms' || 3]
-    this.num_baths = param['num_baths' || 2]
-    this.cost = param['cost' || 320000]
-    this.down_payment = param['down_payment' || 0.20]
-    this.sold = param['sold' || false]
+    this.num_bedrooms = param['num_bedrooms'] || 3
+    this.num_baths = param['num_baths'] || 2
+    this.cost = param['cost'] || 320000
+    this.down_payment = param['down_payment'] || 0.20
+    this.sold = param['sold'] || false
     this.short_sale = param['short_sale']
-    this.has_tenants = param['has_tenants' || false]
+    this.has_tenants = param['has_tenants'] || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '******')
   }
 
   buy(money, good_credit) {
@@ -29,11 +29,18 @@ class House {
   }
 
   to_s() {
-    return `${this.address} : \n
-    ${this.square_feet} sq. ft., \n
-    ${this.num_bedrooms} bed, \n
-    ${this.num_baths} bath. \n
-    ${this.cost}`
+    return `
+    Address initial : ${this.obscure_address()}
+    Address : ${this.address}
+    Square : ${this.square_feet} sq. ft.,
+    Bedroom : ${this.num_bedrooms} bed
+    Num of bath : ${this.num_baths} bath.
+    Cost : ${this.cost}
+    Short Sale? ${this.short_sale}
+    Has Tenant? ${this.has_tenants}
+    Sold : ${this.sold}
+    DP : ${this.down_payment}
+    `
   }
 }
 
@@ -42,9 +49,9 @@ let objParam = {
   square_feet : 100,
   num_bedrooms : 3,
   num_baths : 2,
-  cost : 320000,
+  // cost : , //cost tetap muncul hasil karena || 320000 pada this.cost = param['cost'] || 320000 di constructor
   down_payment : 0.20,
-  sold : true,
+  // sold : true,
   short_sale :  'quick',
   has_tenants : true
 }
