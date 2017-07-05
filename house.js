f484ebbae0a84735d9fe26a2a7b5272d01640c71
adnin31 <adnin.ris31@gmail.com>
2017-07-05 16:47:29 +0700

    argument order telah selsai dikerjakan

--- house.js
+++ house.js
@@ -2,20 +2,20 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(rumah) {
+    this.address = rumah['address']
+    this.square_feet = rumah['square_feet']
+    this.num_bedrooms = rumah['num_bedrooms'||3]
+    this.num_baths = rumah['num_baths'|| 2]
+    this.cost = rumah['cost' || 320000]
+    this.down_payment = rumah['down_payment' || 0.20]
+    this.sold = rumah['sold' || false]
+    this.short_sale = rumah['short_sale']
+    this.has_tenants = rumah['has_tenants' || false]
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
   }
 
   buy(money, good_credit) {
@@ -31,8 +31,23 @@ class House {
   to_s() {
     return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`
   }
+
+  toObj(){
+
+  }
 }
+let newObj = {
+  address :'saya',
+  square_feet : 100,
+  num_bedrooms : 2,
+  num_baths :2,
+  cost :12345,
+  down_payment :12345,
+  sold :true,
+  short_sale :true,
+  has_tenants: false
+}
+const cool = new House(newObj)
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
-
+// console.log(cool.num_bedrooms.replace(/.{10}$/g, '****'));
 console.log(cool.to_s())
